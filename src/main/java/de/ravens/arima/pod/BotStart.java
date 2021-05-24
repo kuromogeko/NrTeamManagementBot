8c879eb77780cc61d7a93aeca965377d30a816be
a <a@a>
2021-05-24 10:59:07 +0200

    The squasher

--- src/main/java/de/ravens/arima/pod/BotStart.java
+++ src/main/java/de/ravens/arima/pod/BotStart.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//todo rebase all
 @EnableAutoConfiguration
 @SpringBootApplication
 public class BotStart {
